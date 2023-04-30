68d231e978be560aa507fd12f05a37554f0eaf58
Maja0426 <tmsmajoros@gmail.com>
2023-04-30 19:46:31 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+// VARIABLES
+const inputField = document.querySelector('#ip-input')
+const result = document.querySelectorAll('.result')
+const btn = document.querySelector('.btn')
+
+var lat = '47.52978'
+var lng = '19.08068'
+
+var map = L.map('map').setView([lat, lng], 13)
+
+async function sendGetRequest() {
+  const response = await fetch(
+    `https://geo.ipify.org/api/v2/country,city?apiKey=at_DmsiDI9h5sPJyuywZ9uJlzXYa2Jd5&ipAddress=${inputField.value}&domain=${inputField.value}`
+  )
+  const data = await response.json()
+  console.log(data)
+  let lat = data.location.lat
+  let lng = data.location.lng
+  result[0].innerText = data.ip
+  result[1].innerText = data.location.city
+  result[2].innerText = data.location.timezone
+  result[3].innerText = data.isp
+  map = L.map('map').setView([lat, lng], 13)
+  L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+    maxZoom: 20,
+    subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+  }).addTo(map)
+
+  var customIcon = L.icon({
+    iconUrl: 'images/icon-location.svg',
+    iconSize: [46, 56],
+  })
+
+  L.marker([lat, lng], { icon: customIcon }).addTo(map)
+}
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault()
+  map.remove()
+  sendGetRequest()
+})
