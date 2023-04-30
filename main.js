6c0f878e0d2c445bd4eac75391251108cca3a45c
Maja0426 <tmsmajoros@gmail.com>
2023-04-30 20:05:11 +0200

    add starter map v2

--- main.js
+++ main.js
@@ -3,16 +3,14 @@ const inputField = document.querySelector('#ip-input')
 const result = document.querySelectorAll('.result')
 const btn = document.querySelector('.btn')
 
-var lat = '47.52978'
-var lng = '19.08068'
+let map = L.map('map').setView([47.52978, 19.08068], 13)
 
-var map = L.map('map').setView([lat, lng], 13)
-sendGetRequest()
+window.addEventListener('DOMContentLoaded', () => {
+  map.remove()
+  sendGetRequest()
+})
 
 async function sendGetRequest() {
-  if (!inputField.value) {
-    inputField.value = '92.249.207.29'
-  }
   const response = await fetch(
     `https://geo.ipify.org/api/v2/country,city?apiKey=at_DmsiDI9h5sPJyuywZ9uJlzXYa2Jd5&ipAddress=${inputField.value}&domain=${inputField.value}`
   )
@@ -29,7 +27,7 @@ async function sendGetRequest() {
     subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
   }).addTo(map)
 
-  var customIcon = L.icon({
+  let customIcon = L.icon({
     iconUrl: 'images/icon-location.svg',
     iconSize: [46, 56],
   })
